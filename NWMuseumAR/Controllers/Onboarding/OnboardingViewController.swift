29730800221bb624b5f288df7758651deeda548d
Justin-Leung <lngventure@gmail.com>
2018-04-09 23:51:45 -0700

    New controller for onboarding, since tutorial used page view which is a bit heavy for what we need here.

---
+++ NWMuseumAR/Controllers/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,14 @@
+//
+//  OnboardingViewController.swift
+//  NWMuseumAR
+//
+//  Created by Justin Leung on 4/9/18.
+//  Copyright © 2018 NWMuseumAR. All rights reserved.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController
+{
+    
+}
